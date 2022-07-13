75f4885ef4853e7290d1e2af3339ebde326806ae
Ethan Salonga <ethansalonga28@gmail.com>
2022-07-13 02:12:48 -0400

    Questions 5-7

--- app.js
+++ app.js
@@ -30,3 +30,39 @@ function arrSum(arr) {
 }
 
 console.log(arrSum([2, 4, 6]))
+
+// Q5. Add up the numbers from a single number
+function progressiveSum(num) {
+  let sum = 0
+  for (let i = 1; i <= num; i++) {
+    sum += i
+  }
+  return sum
+}
+
+console.log(progressiveSum(3))
+
+// Q6. Calculate the time
+function calcTime(seconds) {
+  let timerMinutes = Math.floor(seconds / 60)
+  let timerSeconds = seconds % 60
+
+  if (timerMinutes.toString().length === 1) {
+    timerMinutes = "0" + timerMinutes
+  }
+
+  return timerMinutes + ":" + timerSeconds
+}
+
+console.log(calcTime(70))
+
+// Q7. Find the largest number
+function getMax(arr) {
+  let max = arr[0]
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] > max && (max = arr[i])
+  }
+  return max
+}
+
+console.log(getMax([12, -52, 47, 98, 33]))
